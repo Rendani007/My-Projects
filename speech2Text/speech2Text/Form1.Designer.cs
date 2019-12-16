50c4309c046906bacd1709354ab7c16111e6a55f
rendanni masegwala <rendanni.m@gmail.com>
2019-12-16 21:57:55 +0200

    i added project files

---
+++ speech2Text/speech2Text/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace speech2Text
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Browser = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.convertBtn = new ePOSOne.btnProduct.Button_WOC();
+            this.audioSaveBtn = new ePOSOne.btnProduct.Button_WOC();
+            this.button_WOC5 = new ePOSOne.btnProduct.Button_WOC();
+            this.button_WOC4 = new ePOSOne.btnProduct.Button_WOC();
+            this.recordBtn = new ePOSOne.btnProduct.Button_WOC();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // saveFile
+            // 
+            this.saveFile.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFile_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(581, 296);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Rich Text Format|.rtf";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(568, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(531, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(31, 22);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.convertBtn);
+            this.panel1.Controls.Add(this.audioSaveBtn);
+            this.panel1.Controls.Add(this.button_WOC5);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button_WOC4);
+            this.panel1.Controls.Add(this.recordBtn);
+            this.panel1.Location = new System.Drawing.Point(12, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(587, 497);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.BorderColor = System.Drawing.Color.Orange;
+            this.convertBtn.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.convertBtn.FlatAppearance.BorderSize = 0;
+            this.convertBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.convertBtn.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertBtn.Location = new System.Drawing.Point(402, 339);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.convertBtn.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.convertBtn.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.convertBtn.Size = new System.Drawing.Size(128, 42);
+            this.convertBtn.TabIndex = 16;
+            this.convertBtn.Text = "Convert to text";
+            this.convertBtn.TextColor = System.Drawing.Color.White;
+            this.convertBtn.UseVisualStyleBackColor = false;
+            this.convertBtn.Click += new System.EventHandler(this.Button_WOC2_Click_1);
+            // 
+            // audioSaveBtn
+            // 
+            this.audioSaveBtn.BorderColor = System.Drawing.Color.Orange;
+            this.audioSaveBtn.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.audioSaveBtn.FlatAppearance.BorderSize = 0;
+            this.audioSaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.audioSaveBtn.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.audioSaveBtn.Location = new System.Drawing.Point(228, 339);
+            this.audioSaveBtn.Name = "audioSaveBtn";
+            this.audioSaveBtn.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.audioSaveBtn.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.audioSaveBtn.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.audioSaveBtn.Size = new System.Drawing.Size(128, 42);
+            this.audioSaveBtn.TabIndex = 15;
+            this.audioSaveBtn.Text = "Save";
+            this.audioSaveBtn.TextColor = System.Drawing.Color.White;
+            this.audioSaveBtn.UseVisualStyleBackColor = false;
+            this.audioSaveBtn.Click += new System.EventHandler(this.Button_WOC1_Click_1);
+            // 
+            // button_WOC5
+            // 
+            this.button_WOC5.BorderColor = System.Drawing.Color.Orange;
+            this.button_WOC5.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.button_WOC5.FlatAppearance.BorderSize = 0;
+            this.button_WOC5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_WOC5.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_WOC5.Location = new System.Drawing.Point(312, 411);
+            this.button_WOC5.Name = "button_WOC5";
+            this.button_WOC5.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.button_WOC5.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.button_WOC5.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.button_WOC5.Size = new System.Drawing.Size(128, 42);
+            this.button_WOC5.TabIndex = 14;
+            this.button_WOC5.Text = "Export to text file";
+            this.button_WOC5.TextColor = System.Drawing.Color.White;
+            this.button_WOC5.UseVisualStyleBackColor = false;
+            this.button_WOC5.Click += new System.EventHandler(this.Button_WOC5_Click);
+            // 
+            // button_WOC4
+            // 
+            this.button_WOC4.BorderColor = System.Drawing.Color.Orange;
+            this.button_WOC4.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.button_WOC4.FlatAppearance.BorderSize = 0;
+            this.button_WOC4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_WOC4.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_WOC4.Location = new System.Drawing.Point(140, 411);
+            this.button_WOC4.Name = "button_WOC4";
+            this.button_WOC4.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.button_WOC4.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.button_WOC4.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.button_WOC4.Size = new System.Drawing.Size(128, 42);
+            this.button_WOC4.TabIndex = 13;
+            this.button_WOC4.Text = "Play audio again";
+            this.button_WOC4.TextColor = System.Drawing.Color.White;
+            this.button_WOC4.UseVisualStyleBackColor = false;
+            this.button_WOC4.Click += new System.EventHandler(this.Button_WOC4_Click);
+            // 
+            // recordBtn
+            // 
+            this.recordBtn.BorderColor = System.Drawing.Color.Orange;
+            this.recordBtn.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.recordBtn.FlatAppearance.BorderSize = 0;
+            this.recordBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.recordBtn.Font = new System.Drawing.Font("Segoe UI Historic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recordBtn.Location = new System.Drawing.Point(60, 339);
+            this.recordBtn.Name = "recordBtn";
+            this.recordBtn.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.recordBtn.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.recordBtn.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.recordBtn.Size = new System.Drawing.Size(128, 42);
+            this.recordBtn.TabIndex = 10;
+            this.recordBtn.Text = "Record";
+            this.recordBtn.TextColor = System.Drawing.Color.White;
+            this.recordBtn.UseVisualStyleBackColor = false;
+            this.recordBtn.Click += new System.EventHandler(this.Button_WOC1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(612, 534);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog Browser;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private ePOSOne.btnProduct.Button_WOC recordBtn;
+        private ePOSOne.btnProduct.Button_WOC button_WOC4;
+        private ePOSOne.btnProduct.Button_WOC button_WOC5;
+        private ePOSOne.btnProduct.Button_WOC convertBtn;
+        private ePOSOne.btnProduct.Button_WOC audioSaveBtn;
+    }
+}
+
---
+++ speech2Text/speech2Text/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using NAudio.Wave;
+using System.IO;
+
+namespace speech2Text
+{
+    public partial class Form1 : Form
+    {
+        private BufferedWaveProvider bwp;
+
+        WaveIn waveIn;
+        WaveOut waveOut;
+        WaveFileWriter writer;
+        WaveFileReader reader;
+        string output = "audio.raw";
+        int numlines = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            waveOut = new WaveOut();
+            waveIn = new WaveIn();
+
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveIn_DataAvailable);
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;
+
+            recordBtn.Enabled = true;
+            audioSaveBtn.Enabled = false;
+            convertBtn.Enabled = false;
+        }
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void speechTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recordBtn_Click(object sender, EventArgs e)
+        {
+       
+
+        }
+
+        private void audioSaveBtn_Click(object sender, EventArgs e)
+        {
+ 
+        }
+        private void waveOut_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            waveOut.Stop();
+            reader.Close();
+            reader = null;
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playAudioTxt_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void exportToTxtBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFile_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+       
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            numlines = richTextBox1.Lines.Count();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void Button_WOC1_Click(object sender, EventArgs e)//////Record/////
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                Console.WriteLine("No microphone!");
+                return;
+            }
+
+            waveIn.StartRecording();
+
+            recordBtn.Enabled = false;
+            audioSaveBtn.Enabled = true;
+            convertBtn.Enabled = false;
+
+            // string path = @"C:\\Audio.txt";
+            // File.Create(path);
+        }
+
+     
+
+        private void Button_WOC4_Click(object sender, EventArgs e)//////Play audio again//////
+        {
+            if (File.Exists("audio.raw"))
+            {
+                reader = new WaveFileReader("audio.raw");
+                waveOut.Init(reader);
+                waveOut.Play();
+            }
+            else
+            {
+                MessageBox.Show("No Audio File Found");
+            }
+        }
+
+        private void Button_WOC5_Click(object sender, EventArgs e)/////Export to text file/////
+        {
+            string path = @"C:\Users\Rendi\Documents\Test file\file.txt";
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                int start = 1;
+                while (start <= numlines)
+                {
+                    sr.WriteLine(richTextBox1.Lines[start - 1]);
+                    start++;
+                    MessageBox.Show("Saved to Desktop as file.txt");
+
+                }
+            }
+
+
+
+
+
+
+            /* if (Browser.ShowDialog() == DialogResult.OK)
+            {
+                // create a writer and open the file
+                TextWriter tw = new StreamWriter(Browser.SelectedPath + "Audio.txt");
+                // write a line of text to the file
+                tw.WriteLine(speechTxt.Text);
+                // close the stream
+                tw.Close();
+                MessageBox.Show("Saved to " + Browser.SelectedPath + "\\Audio.txt", "Saved File", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }*/
+
+            /* SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+             saveFileDialog1.InitialDirectory = @"C:\";      
+     saveFileDialog1.Title = "Save text Files";
+             saveFileDialog1.CheckFileExists = false;
+             saveFileDialog1.CheckPathExists = false;
+             saveFileDialog1.DefaultExt = "txt";
+             saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+             saveFileDialog1.FilterIndex = 2;
+             saveFileDialog1.RestoreDirectory = true;
+             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+             {
+                 speechTxt.Text = saveFileDialog1.FileName;
+             }
+             */
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button_WOC1_Click_1(object sender, EventArgs e)/////Save//////
+        {
+            waveIn.StopRecording();
+
+            if (File.Exists("audio.raw"))
+                File.Delete("audio.raw");
+
+            writer = new WaveFileWriter(output, waveIn.WaveFormat);//thaking audiio buffer and writing to disk
+
+            recordBtn.Enabled = false;
+            audioSaveBtn.Enabled = false;
+            convertBtn.Enabled = true;
+
+            byte[] buffer = new byte[bwp.BufferLength];
+            int offset = 0;
+            int count = bwp.BufferLength;
+
+            var read = bwp.Read(buffer, offset, count);
+            if (count > 0)
+            {
+                writer.Write(buffer, offset, read);
+            }
+
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+
+            reader = new WaveFileReader("audio.raw"); // (new MemoryStream(bytes));
+            waveOut.Init(reader);
+            waveOut.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOut_PlaybackStopped);
+            waveOut.Play();
+
+        }
+
+        private void Button_WOC2_Click_1(object sender, EventArgs e)///Convert to text/////
+        {
+            recordBtn.Enabled = true;
+            audioSaveBtn.Enabled = false;
+            convertBtn.Enabled = false;
+
+            if (File.Exists("audio.raw"))
+            {
+
+                var speech = SpeechClient.Create();
+                var response = speech.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    SampleRateHertz = 16000,
+                    LanguageCode = "en",
+                }, RecognitionAudio.FromFile("audio.raw"));
+
+
+                richTextBox1.Text = "";
+
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        richTextBox1.Text = richTextBox1.Text + " " + alternative.Transcript;
+                    }
+                }
+
+                if (richTextBox1.Text.Length == 0)
+                    richTextBox1.Text = "No Data ";
+
+            }
+            else
+            {
+
+                richTextBox1.Text = "Audio File Missing ";
+
+            }
+
+        }
+    }
+}
+   
